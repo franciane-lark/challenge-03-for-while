ea9a7ba6a58641ae835b7b3e46e05085fdb418ff
Franciane Lark <franciane.lark019@gmail.com>
2022-06-14 10:20:41 -0400

    update for/while

---
+++ script.js
@@ -0,0 +1,22 @@
+for (let contador = 0; contador<= 10; contador++){
+    console.log(contador);
+}
+/*---------------------------------------------------*/
+let contador = 10;
+
+while (contador >= 1){
+    console.log(contador);
+    contador--;
+}
+/*---------------------------------------------------*/
+for (let numero = 1; numero <= 100; numero++){
+    if(numero % 2 === 0){
+          console.log(numero);
+     }
+ }
+ /*---------------------------------------------------*/
+ for (let numero = 0; numero < 100; numero++){
+    if(numero % 3 === 0){
+          console.log(numero);
+     }
+ }
